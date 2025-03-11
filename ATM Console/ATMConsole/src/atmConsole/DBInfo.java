23bf944f27021f12a8e3e3aa696922a5dcd8a703
Naniii-code <lokeshmittakanti17@gmail.com>
2025-03-11 11:43:04 +0530

    Add files via upload

---
+++ ATM Console/ATMConsole/src/atmConsole/DBInfo.java
@@ -0,0 +1,14 @@
+package atmConsole;
+
+
+
+public class DBInfo
+
+{
+	public static final String driver="oracle.jdbc.driver.OracleDriver";
+
+   public static final String url="jdbc:oracle:thin:@localhost:1521:orcl";
+   public static final String uName="loki";
+   public static final String pWord="123";
+   
+}
